d105f9cda12a128ec4ef72bd9fd1ecb0d15be4bd
Julian-D-Leal <88947709+Julian-D-Leal@users.noreply.github.com>
2022-12-13 20:49:19 -0500

    Api movies v1.2

--- readme.md
+++ readme.md
@@ -7,7 +7,8 @@ PORT
 
 npm i express express-handlebars mongoose morgan \
 npm i -D @babel/core @babel/cli @babel/node @babel/preset-env nodemon \
-npm i dotenv 
+npm i dotenv \
+npm install cors
 
 ### scripts
 "start": "nodemon src/index.js --exec babel-node"
--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ import express from "express";
 import morgan from "morgan";
 import Moviesroutes from './routes/prueba.routes'
 
-
+var cors = require('cors')
 const app = express();
 
 
 //Middlewares
+app.use(cors())
 app.use(morgan('dev'));//permite ver las peticiones que llegan al servidor
 app.use(express.urlencoded({extended: false}));//permite transformar un objeto a objeto JSON
 app.use(express.json());
